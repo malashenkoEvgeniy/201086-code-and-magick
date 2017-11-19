5c962c2a52bdc5a74b1f05565d365946053de509
malashenkoEvgeniy <malashenko_evgeniy@mail.ua>
2017-11-19 21:01:42 +0200

    error corection

---
+++ js/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+window.fireballSize = 22;
+window.getFireballSpeed = function (left) {
+  return left ? 5 : -2;
+};
+window.wizardSpeed = 3;
+var wizardWidth = 70;
+window.getWizardHeight = function () {
+  return 1.337 * wizardWidth;
+};
+window.getWizardX = function (width) {
+  return width / 2 - wizardWidth / 2;
+};
+window.getWizardY = function (height) {
+  return height * 1 / 3;
+};
