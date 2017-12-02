5f2eb8f8ff21ade1ccefe68ec42c97c3723860fb
malashenkoEvgeniy <malashenko_evgeniy@mail.ua>
2017-12-02 18:23:26 +0200

    test1

--- js/setup.js
+++ js/setup.js
@@ -1,6 +1,4 @@
 'use strict';
-var userDialog = document.querySelector('.setup');
-userDialog.classList.remove('hidden');
 var getFullName = function () {
   var wizardsNames = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
   var name = wizardsNames[Math.floor(Math.random() * wizardsNames.length)];
@@ -47,4 +45,27 @@ var addTemplate = function () {
 };
 addTemplate();
 document.querySelector('.setup-similar').classList.remove('hidden');
+var setupOpen = document.querySelector('.setup-open');
+var setup = document.querySelector('.setup');
+var onAddHiddenWidthKeydown = function (evt) {
+  if (evt.keyCode === 27) {
+    setup.classList.add('hidden');
+  }
+};
+var onRemoweHiddenWidthKeydown = function (evt) {
+  if (evt.keyCode === 13) {
+    setup.classList.remove('hidden');
+  }
+};
+var onRemoveHiddenInSetapOpen = function () {
+  setup.classList.remove('hidden');
+  window.addEventListener('keydown', onAddHiddenWidthKeydown);
+};
+setupOpen.addEventListener('click', onRemoveHiddenInSetapOpen);
+var setupClose = setup.querySelector('.setup-close');
+var onCloseSetup = function () {
+  setup.classList.add('hidden');
+};
+setupOpen.addEventListener('keydown', onRemoweHiddenWidthKeydown);
+setupClose.addEventListener('click', onCloseSetup);
 
