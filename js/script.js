248a5cf2849f3519462e5bb1cf16745b15cc2d29
malashenkoEvgeniy <malashenko_evgeniy@mail.ua>
2017-12-12 19:13:50 +0200

    исправляет ошибки с глобальными переменными

--- js/script.js
+++ js/script.js
@@ -1,17 +1,19 @@
 'use strict';
 
-window.fireballSize = 22;
-window.getFireballSpeed = function (left) {
-  return left ? 5 : -2;
-};
-window.wizardSpeed = 3;
-var wizardWidth = 70;
-window.getWizardHeight = function () {
-  return 1.337 * wizardWidth;
-};
-window.getWizardX = function (width) {
-  return width / 2 - wizardWidth / 2;
-};
-window.getWizardY = function (height) {
-  return height * 1 / 3;
-};
+(function () {
+  window.fireballSize = 22;
+  window.getFireballSpeed = function (left) {
+    return left ? 5 : -2;
+  };
+  window.wizardSpeed = 3;
+  var wizardWidth = 70;
+  window.getWizardHeight = function () {
+    return 1.337 * wizardWidth;
+  };
+  window.getWizardX = function (width) {
+    return width / 2 - wizardWidth / 2;
+  };
+  window.getWizardY = function (height) {
+    return height * 1 / 3;
+  };
+}());
