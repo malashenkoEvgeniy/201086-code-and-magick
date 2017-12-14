67f89397bb1281c17cfeb7c112dc7e6ecace2d6e
malashenkoEvgeniy <malashenko_evgeniy@mail.ua>
2017-12-14 20:59:26 +0200

    модули

--- js/dialog.js
+++ js/dialog.js
@@ -26,6 +26,8 @@
   var closePopup = function () {
     setup.classList.add('hidden');
     document.removeEventListener('keydown', onPopupEscPress);
+    setup.style.top = '';
+    setup.style.left = '';
   };
   setupOpen.addEventListener('click', function () {
     openPopup();
@@ -107,44 +109,5 @@
     document.addEventListener('mousemove', onMouseMove);
     document.addEventListener('mouseup', onMouseUp);
   });
-
-  var shopElement = document.querySelector('.setup-artifacts-shop');
-  var draggedItem = null;
-
-  shopElement.addEventListener('dragstart', function (evt) {
-    if (evt.target.tagName.toLowerCase() === 'img') {
-      draggedItem = evt.target;
-      evt.dataTransfer.setData('text/plain', evt.target.alt);
-      artifactsElement.style.outline = '2px dashed red';
-    }
-  });
-  shopElement.addEventListener('dragend', function () {
-    artifactsElement.style.outline = '';
-  });
-
-  var artifactsElement = document.querySelector('.setup-artifacts');
-
-  artifactsElement.addEventListener('dragover', function (evt) {
-    evt.preventDefault();
-    return false;
-  });
-
-  artifactsElement.addEventListener('drop', function (evt) {
-    evt.target.style.backgroundColor = '';
-    evt.target.appendChild(draggedItem.cloneNode(true));
-    evt.preventDefault();
-  });
-
-
-  artifactsElement.addEventListener('dragenter', function (evt) {
-    evt.target.style.backgroundColor = 'yellow';
-    evt.preventDefault();
-  });
-
-  artifactsElement.addEventListener('dragleave', function (evt) {
-    evt.target.style.backgroundColor = '';
-    evt.preventDefault();
-  });
-
 
 })();
