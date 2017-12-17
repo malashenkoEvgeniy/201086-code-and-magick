8462ab6524adfa07dec353fcead39fd49082d598
malashenkoEvgeniy <malashenko_evgeniy@mail.ua>
2017-12-17 19:05:16 +0200

    adds space

--- js/colorize-element.js
+++ js/colorize-element.js
@@ -1,6 +1,6 @@
 'use strict';
 (function () {
-  window.colorizeElement = function(element, colors, callback) {
+  window.colorizeElement = function (element, colors, callback) {
     var color = colors[Math.floor(Math.random() * colors.length)];
     if (typeof callback === 'function') {
       callback(element, color);
